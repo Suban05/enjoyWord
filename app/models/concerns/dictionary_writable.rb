db767319c638a345274f4b9949ca9b00f0924ba7
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-20 15:13:43 +0500

    add module DictionaryWritable
    there are methods of dictionary for the writing

---
+++ app/models/concerns/dictionary_writable.rb
@@ -0,0 +1,45 @@
+module DictionaryWritable
+  extend ActiveSupport::Concern
+
+  included do
+    validates :name, :translation_type, presence: true
+    enum translation_type: translation_types
+  end
+
+  class_methods do
+    # @param words [String]
+    def write_words(dictionary, words)
+      languages = dictionary.translation_type.to_s.split('_')
+      first_language = language_mappings[languages.first.to_sym]
+      second_language = language_mappings[languages.second.to_sym]
+      word_pairs = words.scan(/(#{first_language.word_template})\s*-\s*(#{second_language.word_template})/)
+      word_pairs.each do |pair|
+        word = dictionary.words.build(content: pair.first, translation: pair.second)
+        word.save
+      end
+    end
+
+    private
+
+    def translation_types
+      languages = language_mappings.keys
+      types = []
+      languages.each do |first|
+        languages.each do |second|
+          next if first == second
+
+          types << "#{first}_#{second}".to_sym
+        end
+      end
+      types
+    end
+
+    def language_mappings
+      {
+        english: English,
+        russian: Russian,
+        spanish: Spanish,
+      }
+    end
+  end
+end
--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,43 +1,8 @@
 require_relative 'languages'
 
 class Dictionary < ApplicationRecord
+  include DictionaryWritable
+
   belongs_to :user
   has_many :words
-  validates :name, :translation_type, presence: true
-
-  # @param words [String]
-  def self.write_words(dictionary, words)
-    languages = dictionary.translation_type.to_s.split('_')
-    first_language = language_mappings[languages.first.to_sym]
-    second_language = language_mappings[languages.second.to_sym]
-    word_pairs = words.scan(/(#{first_language.word_template})\s*-\s*(#{second_language.word_template})/)
-    word_pairs.each do |pair|
-      word = dictionary.words.build(content: pair.first, translation: pair.second)
-      word.save
-    end
-  end
-
-  def self.translation_types
-    languages = language_mappings.keys
-    types = []
-    languages.each do |first|
-      languages.each do |second|
-        next if first == second
-
-        types << "#{first}_#{second}".to_sym
-      end
-    end
-    types
-  end
-
-  def self.language_mappings
-    {
-      english: English,
-      russian: Russian,
-      spanish: Spanish,
-    }
-  end
-
-  enum translation_type: translation_types
-
 end
