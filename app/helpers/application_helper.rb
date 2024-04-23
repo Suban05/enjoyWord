2b93438564ba34738ffb28acffb0188d62465a41
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-23 15:44:17 +0500

    remove old code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,4 @@ module ApplicationHelper
   def prepend_flash
     turbo_stream.prepend 'flash', partial: 'shared/flash'
   end
-
-  def button_dropdown_id(object)
-    "dropdownMenuIconHorizontalButton_#{dom_id(object)}"
-  end
-
-  def data_dropdown_id(object)
-    "dropdownDots_#{dom_id(object)}"
-  end
-
-  def data_modal_id(object)
-    "modal_#{dom_id(object)}"
-  end
 end
