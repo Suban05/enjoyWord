e14f753c90b2199ac03545762cdc697a72d52b4b
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-23 16:10:36 +0500

    rename method 'header_locals' on 'locals'
    add additional options

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -5,7 +5,7 @@ module WordsHelper
     locals:
   end
 
-  def header_locals(list_title_path:, dictionary_id:)
+  def locals(list_title_path:, dictionary_id:, **options)
     {
       list_title_path:,
       create_button_title_path: t('word.index.new_word'),
@@ -13,6 +13,6 @@ module WordsHelper
       load_words_button_link: new_word_loader_path(dictionary_id:),
       search_form: 'words/search_form',
       new_object: Word.new
-    }
+    }.merge(options)
   end
 end
