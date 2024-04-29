d247ccece31fb098f2d64bd2eda070649382c26d
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-29 21:36:47 +0500

    add test of home action

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:john)
+  end
+
   test "should get home" do
-    get pages_home_url
+    log_in_as(@user)
+    get home_path
     assert_response :success
   end
 end
