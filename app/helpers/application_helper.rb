27e80237432e41f07921fcb004b2b3ce2a6bc143
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-29 18:31:37 +0500

    add helper time_ago

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
-  
+
   def prepend_flash
     turbo_stream.prepend 'flash', partial: 'shared/flash'
+  end
+
+  def time_ago(date)
+    return 'Not data' if date.nil?
+    "#{time_ago_in_words(date)} ago"
   end
 end
