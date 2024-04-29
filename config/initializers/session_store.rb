1cee467493b013b2af6377dc21120ccb25744d8c
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-30 01:49:15 +0500

    fix session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store, key: '_enjoy_word',
                                                       expire_after: 1.month,
-                                                      domain: :all,
+                                                      domain: '.onrender.com',
                                                       same_site: :lax
