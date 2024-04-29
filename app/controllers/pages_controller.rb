3b18725624eb2d388ad919870e3b7d4468eabafa
Anatoly Busygin <anatolyb94@gmail.com>
2024-04-29 18:30:38 +0500

    check authentication in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+
   def home
     @last_words = Word.last_added(current_user)
     @popular_dictionaries = Dictionary.popular(current_user)
