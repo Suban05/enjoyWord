c162f52478d8e1eb1a5724401ab7bee61f007e44
Suban05 <anatolyb94@gmail.com>
2024-05-18 20:43:56 +0300

    configure pwa

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'home', to: 'pages#home'
   root 'pages#home'
+
+  get 'home', to: 'pages#home'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   delete 'learn_words_again', to: 'learning_sessions#destroy'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :dictionaries
@@ -13,5 +15,10 @@ Rails.application.routes.draw do
   resources :examples
   resources :word_loaders, only: [:new, :create]
   resources :learning_sessions, only: [:new, :create, :destroy]
+
+   # Render dynamic PWA files from app/views/pwa/*
+   get "service-worker" => "pwa#service_worker", as: :pwa_service_worker
+   get "manifest" => "pwa#manifest", as: :pwa_manifest
+
   match '*unmatched', to: 'application#render_404', via: :all
 end
