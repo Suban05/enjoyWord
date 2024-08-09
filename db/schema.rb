634194c90a8a0f5445f6a9e7bed0d7442c7a4ecf
Suban05 <anatolyb94@gmail.com>
2024-08-09 17:05:26 +0500

    #8 schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_190439) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_09_073958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_10_190439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "favourite_language"
   end
 
   create_table "words", force: :cascade do |t|
