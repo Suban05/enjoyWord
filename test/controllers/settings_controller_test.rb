c97826f35af213c056d6e7051320885606e7dd0f
Suban05 <anatolyb94@gmail.com>
2024-08-09 17:04:20 +0500

    #8 tests of settings

---
+++ test/controllers/settings_controller_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class SettingsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:john)
+  end
+
+  test "should get new" do
+    log_in_as(@user)
+    get new_setting_path
+    assert_response :success
+  end
+
+  test "changes user fields" do
+    log_in_as(@user)
+    assert_equal nil, @user.favourite_language
+    post settings_path params: { email: @user.email, name: @user.name, favourite_language: 'spanish' }
+    assert_redirected_to root_path
+    @user.reload
+    assert_equal 'spanish', @user.favourite_language
+  end
+end
