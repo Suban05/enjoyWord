a74ec90a14f341e492474f060d872599e52aa416
Suban05 <anatolyb94@gmail.com>
2024-08-09 18:41:51 +0500

    #8 get word of the day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,6 @@ class PagesController < ApplicationController
   def home
     @last_words = Word.last_added(current_user)
     @popular_dictionaries = Dictionary.popular(current_user)
+    @word_of_day = current_user.favourite_language_as_object.word_of_day
   end
 end
