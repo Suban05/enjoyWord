303e7bc2d1a3905fd70bf97a548c697122f3d616
Suban05 <anatolyb94@gmail.com>
2024-09-02 20:17:50 +0300

    reduce redundant

--- app/models/dictionary/loadable.rb
+++ app/models/dictionary/loadable.rb
@@ -5,7 +5,9 @@ module Dictionary::Loadable
 
   # @param words [String]
   def write_words(words)
-    word_pairs = WordsLoading::Parsing.new(dictionary: self, words: words).call
-    WordsLoading::Loading.new(dictionary: self, words: word_pairs).call
+    WordsLoading::Loading.new(
+      dictionary: self,
+      words: WordsLoading::Parsing.new(dictionary: self, words: words).call
+    ).call
   end
 end
